605375d05aac7b3348a75af2e1957fded3712a44
usama-hrb <oussama.lharbili@gmail.com>
2025-03-04 13:14:32 +0000

    RE write the rpls

--- cmds/JOIN.d
+++
@@ -1,3 +0,0 @@
-cmds/JOIN.o: cmds/JOIN.cpp cmds/../inc/Server.hpp cmds/../inc/Needs.hpp \
- cmds/../inc/ReplyCodes.hpp cmds/../inc/User.hpp cmds/../inc/Client.hpp \
- cmds/../inc/Channel.hpp
--- cmds/NICK.d
+++
@@ -1,3 +0,0 @@
-cmds/NICK.o: cmds/NICK.cpp cmds/../inc/Needs.hpp \
- cmds/../inc/ReplyCodes.hpp cmds/../inc/User.hpp cmds/../inc/Server.hpp \
- cmds/../inc/Client.hpp cmds/../inc/Channel.hpp
